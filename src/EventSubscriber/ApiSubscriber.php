3dad75adc9d96746d267b62ba1cb3a04d0f75440
Ivan <mr.grl@ya.ru>
2023-12-01 01:31:46 +0400

    Initial commit

--- src/EventSubscriber/ApiSubscriber.php
+++ src/EventSubscriber/ApiSubscriber.php
@@ -5,12 +5,10 @@ namespace SymfonyApiBase\EventSubscriber;
 use Symfony\Component\EventDispatcher\Attribute\AsEventListener;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\HttpKernel\Event\ControllerEvent;
 use Symfony\Component\HttpKernel\Event\ExceptionEvent;
 use Symfony\Component\HttpKernel\KernelEvents;
 
 #[AsEventListener(event: KernelEvents::EXCEPTION, method: 'onApiException')]
-#[AsEventListener(event: KernelEvents::CONTROLLER, method: 'onController')]
 class ApiSubscriber
 {
     public function onApiException(ExceptionEvent $event): void
@@ -27,18 +25,5 @@ class ApiSubscriber
             'message' => $exception->getMessage(),
             'data'    => []
         ], $exception->statusCode ?? Response::HTTP_BAD_REQUEST));
-    }
-
-    public function onController(ControllerEvent $event): void
-    {
-        $request = $event->getRequest();
-        if (!str_starts_with($request->getPathInfo(), '/api/')) {
-            return;
-        }
-
-        $authorizationHeader = $request->headers->get('Authorization');
-        if (!$authorizationHeader || !preg_match('/Bearer\s+(.+)/i', $authorizationHeader, $matches)) {
-            return;
-        }
     }
 }
