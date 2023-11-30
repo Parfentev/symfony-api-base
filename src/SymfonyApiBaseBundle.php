dbadca55803d088d589d27b2fbb0069eb33de5c1
Ivan <mr.grl@ya.ru>
2023-11-30 17:53:58 +0400

    Initial commit

---
+++ src/SymfonyApiBaseBundle.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace SymfonyApiBase;
+
+use Symfony\Component\HttpKernel\Bundle\AbstractBundle;
+
+/**
+ * Bundle.
+ *
+ * @author Ivan Parfentev
+ */
+class SymfonyApiBaseBundle extends AbstractBundle
+{
+}
